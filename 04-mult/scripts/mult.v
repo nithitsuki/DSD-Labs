6761ab6ed0aebb1dc6611ef73af5554830f8e94e
Nitin Chandrachoodan <nitin@ee.iitm.ac.in>
2025-05-14 09:09:35 +0530

    Added mult.v for problem 4

---
+++ 04-mult/scripts/mult.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/11/2025 03:34:08 PM
+// Design Name: 
+// Module Name: mult
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mult(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] c
+    );
+    assign c = a * b;
+endmodule
