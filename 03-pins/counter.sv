a93f3437eb241152d199ff5f193e63d6cf9a7193
nithitsuki <nithilanr@gmail.com>
2025-09-06 12:22:26 +0530

    Qn3 - Pins

--- 03-pins/counter.sv
+++ 03-pins/counter.sv
@@ -1,5 +1,6 @@
 module counter (
     input logic clkpulse,       // Clock input
+    input logic [1:0] sw,   // Direction control (1 = up, 0 = down)
     input logic rst,         // Active low reset
     output logic [3:0] led // 4-bit counter output
 );
@@ -8,7 +9,11 @@ module counter (
         if (rst) begin
             led <= 4'b0000; // Reset counter to 0
         end else begin
-            led <= led + 1; // Increment counter
+            if (sw == 1'b1) begin
+                led <= led + 1'b1; 
+            end else begin
+                led <= led - 1'b1;
+            end
         end
     end
 
